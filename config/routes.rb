47283c5c24aeb6ec1bedf25316a82f2375e97698
tabula9 <fhmtt484@gmail.com>
2023-06-07 07:31:16 +0900

    first

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'users/index'
+  get 'lessons/hello'
+  get 'lessons/call', to:'lessons#call'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
+end
